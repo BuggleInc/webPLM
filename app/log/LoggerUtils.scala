79062db63585156e786cad9e5ae6f6414688cf71
Matthieu NICOLAS <matthieu.nicolas@telecomnancy.net>
2015-03-31 14:00:17 +0200

    Remove call to Game.getInstance

--- app/log/LoggerUtils.scala
+++ app/log/LoggerUtils.scala
@@ -4,29 +4,29 @@ import play.Logger
 import plm.core.model.Game
 
 object LoggerUtils {
-  val GAME = Game.getInstance
+  //val GAME = Game.getInstance
   
   def trace(s: String) {
-    GAME.setCaptureOutput(false)
+    //GAME.setCaptureOutput(false)
     Logger.trace(s)
-    GAME.setCaptureOutput(true)
+    //GAME.setCaptureOutput(true)
   }
   
   def debug(s: String) {
-    GAME.setCaptureOutput(false)
+    //GAME.setCaptureOutput(false)
     Logger.debug(s)
-    GAME.setCaptureOutput(true)
+    //GAME.setCaptureOutput(true)
   }
   
   def warn(s: String) {
-    GAME.setCaptureOutput(false)
+    //GAME.setCaptureOutput(false)
     Logger.warn(s)
-    GAME.setCaptureOutput(true)
+    //GAME.setCaptureOutput(true)
   }
   
   def error(s: String) {
-    GAME.setCaptureOutput(false)
+    //GAME.setCaptureOutput(false)
     Logger.error(s)
-    GAME.setCaptureOutput(true)
+    //GAME.setCaptureOutput(true)
   }
 }
