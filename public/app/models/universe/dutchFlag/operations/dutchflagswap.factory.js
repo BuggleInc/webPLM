919e8e57b67861ce9390ec69ebcd6fda7cff77d0
Théodore lambolez <theodore.lambolez@gmail.com>
2015-04-20 10:56:58 +0200

    add dutchflagswap factory

---
+++ public/app/models/universe/dutchFlag/operations/dutchflagswap.factory.js
@@ -0,0 +1,35 @@
+(function ()
+{
+	'use strict';
+
+	angular
+		.module('PLMApp')
+		.factory('DutchFlagSwap', DutchFlagSwap);
+
+	function DutchFlagSwap()
+	{	
+		var DutchFlagSwap = function(data)
+		{
+			this.dest = data.destination;
+			this.src = data.source;
+		};
+
+		DutchFlagSwap.prototype.apply = function(currentWorld)
+		{
+			var tmp = currentWorld.content[this.src];
+			currentWorld.content[this.src] = currentWorld.content[this.dest];
+			currentWorld.content[this.dest] = tmp;
+
+			currentWorld.moveCount += 1;
+		};
+
+		DutchFlagSwap.prototype.reverse = function(currentWorld)
+		{
+			var tmp = currentWorld.content[this.dest];
+			currentWorld.content[this.dest] = currentWorld.content[this.src];
+			currentWorld.content[this.src] = tmp ;
+		};
+
+		return DutchFlagSwap;
+	}
+})
