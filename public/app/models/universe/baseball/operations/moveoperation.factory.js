ff95ea398a8da7890c822627e0da0f7b531bea1e
Théodore lambolez <theodore.lambolez@gmail.com>
2015-04-30 11:07:58 +0200

    fix reverse for moveoperation

--- public/app/models/universe/baseball/operations/moveoperation.factory.js
+++ public/app/models/universe/baseball/operations/moveoperation.factory.js
@@ -10,14 +10,12 @@
 		{
 			this.base = data.base;
 			this.position = data.position;
-			this.oldBase = data.oldBase;
-			this.oldPosition = data.oldPosition;
 		}
 
 		MoveOperation.prototype.apply = function (currentWorld)
 		{
-			this.oldBase = this.base;
-			this.oldPosition = this.position;
+			this.oldBase = currentWorld.holeBase;
+			this.oldPosition = currentWorld.holePos;
 			currentWorld.field[currentWorld.holeBase*currentWorld.posAmount + currentWorld.holePos] = currentWorld.field[this.base * currentWorld.posAmount + this.position];
 			currentWorld.field[this.base * currentWorld.posAmount + this.position] = -1;
 			currentWorld.holeBase = this.base;
@@ -27,9 +25,10 @@
 
 		MoveOperation.prototype.reverse = function(currentWorld)
 		{
-			
-			currentWorld.field[currentWorld.holeBase*currentWorld.posAmount + currentWorld.holePos] = currentWorld.field[[this.oldBase*currentWorld.posAmount+this.oldPosition]];
-			currentWorld.field[this.oldBase*currentWorld.posAmount+this.oldPosition] = -1;
+			currentWorld.field[this.base*currentWorld.posAmount + this.position] = currentWorld.field[this.oldBase*currentWorld.posAmount + this.oldPosition];
+			currentWorld.field[this.oldBase*currentWorld.posAmount + this.oldPosition] = -1;
+			currentWorld.holeBase = this.oldBase;
+			currentWorld.holePos = this.oldPosition;
 			currentWorld.moveCount--;
 
 		};
