3f9d766547beb795f09e3fe7f6484f28924a6131
Théodore lambolez <theodore.lambolez@gmail.com>
2015-05-06 15:51:21 +0200

    add drawBases for secondview baseball

--- public/app/models/universe/baseball/baseballsecondview.factory.js
+++ public/app/models/universe/baseball/baseballsecondview.factory.js
@@ -22,17 +22,41 @@
 			ctx = canvas.getContext('2d');
 			canvasWidth = canvas.width;
 			canvasHeight = canvas.height;
-		};
+		}
 
 		function draw(canvas, baseballWorld)
 		{
 			initUtils(canvas, baseballWorld);
 			ctx.beginPath();
 			ctx.strokeStyle = '#000000';
-			//draws edge of our drawArena
+			//draws edges of our drawArena
 			ctx.strokeRect(0,0, canvasWidth, canvasHeight);
+			drawBases(baseballWorld);
 			ctx.closePath();
-		};
+		}
+
+		function drawBases(baseballWorld)
+		{
+			var nb = baseballWorld.baseAmount;
+			var nbPos = baseballWorld.posAmount;
+			
+			var width = 15;
+			var height = 60 + (5 * nbPos) - (3.5 * nb);
+			var spacing = (canvasHeight - (height * nb)) / nb;
+
+			var x, y;
+
+			for(var i = 0; i < nb; i++)
+			{
+				y = i * (spacing + height);
+				x = canvasWidth - width;
+				ctx.beginPath();
+				ctx.fillStyle = baseballWorld.colors[i];
+				ctx.fillRect(0, y, width, height);
+				ctx.fillRect(x,y,width,height);
+				ctx.closePath();
+			}
+		}
 
 		
 
