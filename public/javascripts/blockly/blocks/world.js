5a08c234dbbb91205d1d52274359ea831d7e4678
Baptiste Mounier <baptiste.mounier@hotmail.fr>
2015-06-05 13:10:25 +0200

    Fix, names errors for translation

--- public/javascripts/blockly/blocks/world.js
+++ public/javascripts/blockly/blocks/world.js
@@ -2,7 +2,7 @@ Blockly.Blocks['world_ground_color'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_GROUND_COLOR_TITLE);
+            .appendField(Blockly.Msg.WORLD_GROUND_COLOR_TITLE);
         this.setOutput(true, 'Color');
         this.setTooltip(Blockly.Msg.WORLD_GROUND_COLOR_TOOLTIP);
     }
@@ -12,7 +12,7 @@ Blockly.Blocks['world_baggle_ground'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_BAGGLE_GROUND_TITLE);
+            .appendField(Blockly.Msg.WORLD_BAGGLE_GROUND_TITLE);
         this.setOutput(true, 'Boolean');
         this.setTooltip(Blockly.Msg.WORLD_BAGGLE_GROUND_TOOLTIP);
     }
@@ -22,7 +22,7 @@ Blockly.Blocks['world_baggle_bag'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_BAGGLE_BAG_TITLE)
+            .appendField(Blockly.Msg.WORLD_BAGGLE_BAG_TITLE)
         this.setOutput(true, 'Boolean');
         this.setTooltip(Blockly.Msg.WORLD_BAGGLE_BAG_TOOLTIP);
     }
@@ -32,7 +32,7 @@ Blockly.Blocks['world_baggle_pickup'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_BAGGLE_PICKUP_TITLE);
+            .appendField(Blockly.Msg.WORLD_BAGGLE_PICKUP_TITLE);
         this.setPreviousStatement(true);
         this.setNextStatement(true);
         this.setTooltip(Blockly.Msg.WORLD_BAGGLE_PICKUP_TOOLTIP);
@@ -43,7 +43,7 @@ Blockly.Blocks['world_baggle_drop'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_BAGGLE_DROP_TITLE);
+            .appendField(Blockly.Msg.WORLD_BAGGLE_DROP_TITLE);
         this.setPreviousStatement(true);
         this.setNextStatement(true);
         this.setTooltip(Blockly.Msg.WORLD_BAGGLE_DROP_TOOLTIP);
@@ -54,7 +54,7 @@ Blockly.Blocks['world_message_ground'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_MESSAGE_GROUND_TITLE);
+            .appendField(Blockly.Msg.WORLD_MESSAGE_GROUND_TITLE);
         this.setOutput(true, 'Boolean');
         this.setTooltip(Blockly.Msg.WORLD_MESSAGE_GROUND_TOOLTIP);
     }
@@ -64,7 +64,7 @@ Blockly.Blocks['world_message_write'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_MESSAGE_WRITE_TITLE);
+            .appendField(Blockly.Msg.WORLD_MESSAGE_WRITE_TITLE);
         this.appendValueInput("VAL")
             .setCheck("String");
         this.setInputsInline(true);
@@ -78,7 +78,7 @@ Blockly.Blocks['world_message_read'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_MESSAGE_READ_TITLE);
+            .appendField(Blockly.Msg.WORLD_MESSAGE_READ_TITLE);
         this.setOutput(true, 'String');
         this.setTooltip(Blockly.Msg.WORLD_MESSAGE_READ_TOOLTIP);
     }
@@ -88,7 +88,7 @@ Blockly.Blocks['world_message_clear'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_MESSAGE_CLEAR_TITLE);
+            .appendField(Blockly.Msg.WORLD_MESSAGE_CLEAR_TITLE);
         this.setPreviousStatement(true);
         this.setNextStatement(true);
         this.setTooltip(Blockly.Msg.WORLD_MESSAGE_CLEAR_TOOLTIP);
@@ -99,9 +99,9 @@ Blockly.Blocks['world_crossing'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.);
+            .appendField(Blockly.Msg.WORLD_CROSSING_TITLE);
         this.setOutput(true, 'Boolean');
-        this.setTooltip('');
+        this.setTooltip(Blockly.Msg.WORLD_CROSSING_TOOLTIP);
     }
 };
 
@@ -109,9 +109,9 @@ Blockly.Blocks['world_exitReached'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.WORLD_CROSSING_TITLE);
+            .appendField(Blockly.Msg.WORLD_EXITREACHED_TITLE);
         this.setOutput(true, 'Boolean');
-        this.setTooltip(Blockly.Msg.WORLD_CROSSING_TOOLTIP);
+        this.setTooltip(Blockly.Msg.WORLD_EXITREACHED_TOOLTIP);
     }
 };
 
@@ -119,9 +119,9 @@ Blockly.Blocks['world_ground_white'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_GROUND_WHITE_TITLE);
+            .appendField(Blockly.Msg.WORLD_GROUND_WHITE_TITLE);
         this.setOutput(true, 'Boolean');
-        this.setTooltip(Blockly.Msg.WOLRD_GROUND_WHITE_TOOLTIP);
+        this.setTooltip(Blockly.Msg.WORLD_GROUND_WHITE_TOOLTIP);
     }
 };
 
@@ -129,7 +129,7 @@ Blockly.Blocks['world_ground_orange'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_GROUND_ORANGE_TITLE);
+            .appendField(Blockly.Msg.WORLD_GROUND_ORANGE_TITLE);
         this.setOutput(true, 'Boolean');
         this.setTooltip(Blockly.Msg.WORLD_GROUND_ORANGE_TOOLTIP);
     }
@@ -139,7 +139,7 @@ Blockly.Blocks['world_height_get'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_HEIGHT_GET_TITLE);
+            .appendField(Blockly.Msg.WORLD_HEIGHT_GET_TITLE);
         this.setOutput(true, 'Number');
         this.setTooltip(Blockly.Msg.WORLD_HEIGHT_GET_TOOLTIP);
     }
@@ -149,7 +149,7 @@ Blockly.Blocks['world_width_get'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_WIDTH_GET_TITLE);
+            .appendField(Blockly.Msg.WORLD_WIDTH_GET_TITLE);
         this.setOutput(true, 'Number');
         this.setTooltip(Blockly.Msg.WORLD_WIDTH_GET_TOOLTIP);
     }
@@ -159,7 +159,7 @@ Blockly.Blocks['world_indication_get'] = {
     init: function () {
         this.setColour(60);
         this.appendDummyInput()
-            .appendField(Blockly.Msg.WOLRD_INDICATION_GET_TITLE);
+            .appendField(Blockly.Msg.WORLD_INDICATION_GET_TITLE);
         this.setOutput(true, 'Char');
         this.setTooltip(Blockly.Msg.WORLD_INDICATION_GET_TOOLTIP);
     }
