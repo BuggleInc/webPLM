32222d5341f97956fe046f8166d2084effeaf44b
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-07-01 10:17:23 +0200

    Debug success modal

--- public/app/components/success-modal.directive.js
+++ public/app/components/success-modal.directive.js
@@ -5,11 +5,14 @@
 		.module('PLMApp')
 		.directive('successModal', successModal);
 	
-	function successModal () {
+  successModal.$inject = ['exercisesList'];
+  
+	function successModal (exercisesList) {
 		return {
 			restrict: 'E',
 			templateUrl: '/assets/app/components/success-modal.directive.html',
 			link: function (scope, element, attrs) {
+        scope.exercisesList = exercisesList;
 				$(document).foundation('reveal', 'reflow');
 			}
 		};
