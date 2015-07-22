9125c6d7e8c6ed49f4751d473e2e11b35fede8be
Tanguy Gloaguen <tanguy.gloaguen@telecomnancy.net>
2015-07-22 10:15:55 +0200

    Shared Git.

--- app/models/PLM.scala
+++ app/models/PLM.scala
@@ -25,8 +25,8 @@ class PLM(userUUID: String, plmLogger: PLMLogger, locale: Locale, lastProgLang: 
   
   var _currentExercise: Exercise = _
   var _currentLang: Lang = _
-  var game = new Game(userUUID, plmLogger, locale, lastProgLang.getOrElse("Java"), trackUser)
   var gitGest = new Git(game, userUUID)
+  var game = new Game(userUUID, plmLogger, locale, lastProgLang.getOrElse("Java"), git.gitUtils, trackUser)
   var tribunal : Tribunal = new Tribunal
   
   def lessons: Array[Lesson] = game.getLessons.toArray(Array[Lesson]())
