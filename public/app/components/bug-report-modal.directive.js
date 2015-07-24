ebe0c8dea9bdd1b92c9ba0180f48f69750512063
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-07-24 09:35:32 +0200

    Update client-side form to handle & display errors

--- public/app/components/bug-report-modal.directive.js
+++ public/app/components/bug-report-modal.directive.js
@@ -5,18 +5,28 @@
 		.module('PLMApp')
 		.directive('bugReportModal', bugReportModal);
 	
-  bugReportModal.$inject = ['bugReport'];
+  bugReportModal.$inject = ['listenersHandler', 'bugReport'];
   
-	function bugReportModal (bugReport) {
+	function bugReportModal (listenersHandler, bugReport) {
 		return {
 			restrict: 'E',
 			templateUrl: '/assets/app/components/bug-report-modal.directive.html',
 			link: function (scope, element, attrs) {
         scope.bugReport = bugReport;
-        console.log('scope: ', scope);
+        listenersHandler.register('onmessage', handleMessage);
         $(document).foundation('alert', 'reflow');
 				$(document).foundation('reveal', 'reflow');
 			}
 		};
 	}
+  
+  function handleMessage(data) {
+      var cmd = data.cmd;
+      var args = data.args;
+      switch (cmd) {
+        case 'issueCreated':
+          $('#bugReportModal').foundation('reveal', 'close');
+          break;
+      }
+  }
 })();
--- public/app/services/bugreport.service.js
+++ public/app/services/bugreport.service.js
@@ -5,24 +5,56 @@
 		.module('PLMApp')
 		.factory('bugReport', bugReport);
 	
-  bugReport.$inject = ['connection'];
+  bugReport.$inject = ['connection', 'listenersHandler', 'gettextCatalog', 'toasterUtils'];
   
-	function bugReport(connection, gettextCatalog) {
-    var title = '';
-    var text = '';
-
-		var service = {
-      title: title,
-      text: text,
+	function bugReport(connection, listenersHandler, gettextCatalog, toasterUtils) {
+    var NO_ERROR = -1;
+    var INCORRECT_ISSUE = 0;
+    var ERRORED_ISSUE = 1;
+    
+    listenersHandler.register('onmessage', handleMessage);
+        
+	var service = {
+      title: '',
+      body: '',
+      error: NO_ERROR,
+      errorTitle: '',
+      errorMsg: '',
+      issuesUrl: 'https://github.com/BuggleInc/webPLM/issues',
+      INCORRECT_ISSUE: INCORRECT_ISSUE,
+      ERRORED_ISSUE: ERRORED_ISSUE,
       submit: submit
     };
 
-		return service;
+	return service;
 
     function submit() {
-      connection.sendMessage('submitBugReport', { title: title, text: text });
-      title = '';
-      text = '';
+      service.error = NO_ERROR;
+      service.errorTitle = '';
+      service.errorMsg = '';
+      connection.sendMessage('submitBugReport', { title: service.title, body: service.body });
     }
+    
+    function handleMessage(data) {
+      var cmd = data.cmd;
+      var args = data.args;
+      switch (cmd) {
+        case 'incorrectIssue':
+          service.error = INCORRECT_ISSUE;
+          service.errorMsg = args.msg;
+          break;
+        case 'issueErrored':
+          service.error = ERRORED_ISSUE;
+          break;
+        case 'issueCreated':
+          var toastTitle = gettextCatalog.getString('Issue created');
+          var toastMsg = gettextCatalog.getString('Thank you for your remark, we will do our best to integrate it. Follow our progress at {{url}}', { url: args.url });
+          toasterUtils.success(toastTitle, toastMsg, -1);
+          service.title = '';
+          service.body = '';
+          service.error = NO_ERROR;
+          break;
+      }
+    }    
 	}
 })();
