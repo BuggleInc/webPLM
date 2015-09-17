eae14092180538cf16cb05367d86c87f91faa4d0
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-09-17 15:58:53 +0200

    Add objects to convert TurtleWorld's Java model to JSON

--- app/json/world/SortWorldToJson.scala
+++ app/json/world/SortWorldToJson.scala
@@ -2,7 +2,6 @@ package json.world
 
 import play.api.libs.json._
 import plm.universe.sort.SortingWorld
-import javax.swing.plaf.basic.BasicListUI.Actions
 import plm.universe.sort.Action
 
 object SortWorldToJson {
