9c92723b3baf5d8dc4f23bca24bc6e3e82340137
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-11-13 15:56:00 +0100

    Add GitActor.generateCommitMessage()

--- app/actors/GitActor.scala
+++ app/actors/GitActor.scala
@@ -2,15 +2,16 @@ package actors
 
 import java.io.File
 import java.io.PrintWriter
-
 import scala.collection.mutable.HashMap
-
 import akka.actor.Actor
 import akka.actor.Props
 import plm.core.lang.ProgrammingLanguage
 import plm.core.model.lesson.ExecutionProgress
+import plm.core.model.lesson.ExecutionProgress.outcomeKind._
 import plm.core.model.lesson.Exercise
 import utils.FileUtils
+import play.api.libs.json.{ JsObject, Json }
+import play.api.Logger
 
 /**
  * @author matthieu
@@ -34,7 +35,8 @@ class GitActor(gitID: String) extends Actor {
       sender ! getCode(exerciseID, progLang)
     case Executed(exercise: Exercise, result: ExecutionProgress, code: String, humanLang: String) =>
       createFiles(exercise, result, code, humanLang)
-      // generateCommitMessage
+      val commitMessage = generateCommitMessage("executed", Some(exercise), None, Some(result), None)
+      Logger.error("commit message: "+commitMessage)
       // commit
     case _ =>
   }
@@ -48,6 +50,59 @@ class GitActor(gitID: String) extends Actor {
   def retrieveCodePath(exerciseID: String, progLang: ProgrammingLanguage): String = {
     val filename: String = List(exerciseID, progLang.getExt, "code").mkString(".")
     return List(home, gitDirectory, gitID, filename).mkString("/")
+  }
+
+  def generateCommitMessage(eventKind: String, optExercise: Option[Exercise], optExoTo: Option[Exercise], optResult: Option[ExecutionProgress], optDefaultJson: Option[JsObject]): String = {
+    var json: JsObject = Json.obj()
+    
+    optDefaultJson match {
+    case Some(defaultJson: JsObject) =>
+      json = defaultJson
+    case _ =>
+    }
+    
+    optExercise match {
+    case Some(exercise: Exercise) =>
+      json = json ++ Json.obj("exo" -> exercise.getId)
+    case _ =>
+    }
+
+    optResult match {
+    case Some(result: ExecutionProgress) =>
+      var outcome: String = "UNKNOWN"
+      result.outcome match {
+      case COMPILE =>
+        outcome = "compile"
+      case FAIL =>
+        outcome = "fail"
+      case PASS =>
+        outcome = "pass"
+      }
+
+      json = json ++ Json.obj(
+        "lang" -> result.language.toString,
+        "outcome" -> outcome,
+        "passedtests" -> result.passedTests,
+        "totaltests" -> result.totalTests
+      )
+
+      if (result.feedbackDifficulty != null)
+        json = json ++ Json.obj("exoDifficulty" -> result.feedbackDifficulty)
+      if (result.feedbackInterest != null)
+        json = json ++ Json.obj("exoInterest" -> result.feedbackInterest)
+      if (result.feedback != null)
+        json = json ++ Json.obj("exoComment" -> result.feedback)
+    case _ =>
+    }
+
+    optExoTo match {
+    case Some(exoTo: Exercise) =>
+      json = json ++ Json.obj("switchto" -> exoTo.getId)
+    case _ =>
+    }
+
+    // Misuses JSON to ensure that the kind is always written first so that we can read github commit lists
+    return "{\"kind\": \"" + eventKind + "\", " + json.toString.substring(1)
   }
 
   def createFiles(exercise: Exercise, result: ExecutionProgress, code: String, humanLang: String) {
