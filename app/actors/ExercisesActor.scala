b5fe206d6bed259b0b7d9fb480b5f12359605d18
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-12-07 16:39:47 +0100

    Add function to browse files to generate exercises list

--- app/actors/ExercisesActor.scala
+++ app/actors/ExercisesActor.scala
@@ -19,6 +19,8 @@ import json.world.WorldToJson
 import plm.core.lang.LangJava
 import utils.LangUtils
 import models.ProgrammingLanguages
+import scala.util.matching.Regex
+import java.io.File
 
 /**
  * @author matthieu
@@ -26,9 +28,11 @@ import models.ProgrammingLanguages
 
 object ExercisesActor {
   def props = Props[ExercisesActor]
-  val exercisesName: Array[String] = Array( // WARNING, keep ChooseLessonDialog.lessons synchronized
-    "Environment"
-  )
+
+  val baseDirectory: File = new File("exercises")
+  val filterRegexp = new Regex("^(.(?!Entity))*\\.java$") // Select all files ending with ".java" but not containing "Entity"
+
+  val exercisesName: Array[String] = generateExercisesIDsList(baseDirectory, filterRegexp)
 
   val logger: PLMLogger = new PLMLogger
   val i18n: I18n = I18nFactory.getI18n(getClass(),"org.plm.i18n.Messages", new Locale("en"), I18nFactory.FALLBACK);
@@ -39,6 +43,25 @@ object ExercisesActor {
   val exercises: Map[String, Exercise] = initExercises
 
   case class GetExercise(exerciseID: String)
+
+  def generateExercisesIDsList(directory: File, r: Regex): Array[String] = {
+    val files: Array[File] = directory.listFiles
+    var results: Array[String] = Array()
+    if(files != null) {
+      results = results ++ files.filter(_.isDirectory).flatMap(recursiveListFiles(_,r)).map { file => file }
+    }
+    results
+  }
+
+  def recursiveListFiles(f: File, r: Regex): Array[String] = {
+    val files: Array[File] = f.listFiles
+    var results: Array[String] = Array()
+    if(files != null) {
+      results = files.filter(f => r.findFirstIn(f.getName).isDefined).map { file => f.getName + "." + file.getName.dropRight(5) /* To remove ".java" */ }
+      results = results ++ files.filter(_.isDirectory).flatMap(recursiveListFiles(_,r)).map { file => f.getName + "." + file }
+    }
+    results
+  }
 
   def initHumanLanguages(): Array[Locale] = {
     var humanLanguages: Array[Locale] = Array()
@@ -51,9 +74,11 @@ object ExercisesActor {
   def initExercises(): Map[String, Exercise] = {
     var exercises: Map[String, Exercise] = Map()
 
-    var exercise: Exercise = Class.forName("environment.Environment").getDeclaredConstructor().newInstance().asInstanceOf[Exercise]
-    exercisesFactory.initializeExercise(exercise, ProgrammingLanguages.defaultProgrammingLanguage)
-    exercises += ("Environment" -> exercise)
+    exercisesName.foreach { exerciseName => 
+      val exercise: Exercise = Class.forName(exerciseName).getDeclaredConstructor().newInstance().asInstanceOf[Exercise]
+      exercisesFactory.initializeExercise(exercise, ProgrammingLanguages.defaultProgrammingLanguage)
+      exercises += (exerciseName -> exercise)
+    }
 
     exercises
   }
@@ -70,7 +95,7 @@ class ExercisesActor extends Actor {
   }
   
   def getExercise(exerciseID: String): Exercise = {
-    val exercise: Exercise = exercises.get("Environment").get
+    val exercise: Exercise = exercises.get(exerciseID).get
     exercisesFactory.cloneExercise(exercise)
   }
 }
