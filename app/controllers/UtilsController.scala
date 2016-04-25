c6b16755caa9aa62c17aa305267bcfe005ddd950
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-04-25 17:00:58 +0200

    Add route to export exercises as JSON

---
+++ app/controllers/UtilsController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import actors.ExercisesActor
+import play.api.mvc._
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import com.google.inject.Inject
+
+class UtilsController @Inject() (system: ActorSystem) extends Controller {
+  
+  val exercisesActor: ActorRef = system.actorOf(ExercisesActor.props)
+  
+  def exportExercises = Action {
+    exercisesActor ! ExercisesActor.ExportExercises
+    Ok("It's done")
+  }
+}
