355c2a9df448ee4a199252b2efe96de589a72e5b
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-05-18 16:06:28 +0200

    Update Turtle universe's model to fit to Jackson config

--- public/app/models/universe/turtle/turtleworld.factory.js
+++ public/app/models/universe/turtle/turtleworld.factory.js
@@ -1,33 +1,34 @@
-(function () {
-	'use strict';
+(function() {
+  'use strict';
 
-	angular
-		.module('PLMApp')
-		.factory('TurtleWorld', TurtleWorld);
+  angular
+    .module('PLMApp')
+    .factory('TurtleWorld', TurtleWorld);
 
-	TurtleWorld.$inject = ['Turtle',
-                        'Line', 'Circle',
-                        'SizeHint',
-                        'MoveTurtle', 'RotateTurtle', 'ChangeTurtleVisible',
-                        'AddLine', 'AddCircle', 'ClearCanvas',
-                        'AddSizeHint'];
+  TurtleWorld.$inject = ['Turtle',
+    'Line', 'Circle',
+    'SizeHint',
+    'MoveTurtle', 'RotateTurtle', 'ChangeTurtleVisible',
+    'AddLine', 'AddCircle', 'ClearCanvas',
+    'AddSizeHint'
+  ];
 
-	function TurtleWorld(Turtle,
-                        Line, Circle,
-                        SizeHint,
-                        MoveTurtle, RotateTurtle, ChangeTurtleVisible,
-                        AddLine, AddCircle, ClearCanvas,
-                        AddSizeHint) {
+  function TurtleWorld(Turtle,
+    Line, Circle,
+    SizeHint,
+    MoveTurtle, RotateTurtle, ChangeTurtleVisible,
+    AddLine, AddCircle, ClearCanvas,
+    AddSizeHint) {
 
-		var TurtleWorld = function (world) {
-      var turtleID, turtle;
-      
-			this.type = world.type;
-			this.width = world.width;
-			this.height = world.height;
-			this.operations = [];
-			this.currentState = -1;
-			this.steps = [];
+    var TurtleWorld = function(world) {
+      var turtleID, turtle, i;
+
+      this.type = world.type;
+      this.width = world.width;
+      this.height = world.height;
+      this.operations = [];
+      this.currentState = -1;
+      this.steps = [];
 
       this.shapes = [];
       this.addShapes(world.shapes);
@@ -36,98 +37,108 @@
       this.addSizeHints(world.sizeHints);
 
       this.entities = {};
-			for (turtleID in world.entities) {
-				if (world.entities.hasOwnProperty(turtleID)) {
-					turtle = world.entities[turtleID];
-					this.entities[turtleID] = new Turtle(turtle);
-				}
-			}
-		};
 
-    TurtleWorld.prototype.getEntity = function (entityID) {
-			return this.entities[entityID];
-		};
-    
-		TurtleWorld.prototype.clone = function () {
-			return new TurtleWorld(this);
-		};
+      if (world instanceof TurtleWorld) {
+        for (turtleID in world.entities) {
+          if (world.entities.hasOwnProperty(turtleID)) {
+            turtle = world.entities[turtleID];
+            this.entities[turtleID] = new Turtle(turtle);
+          }
+        }
+      } else {
+        for (i = 0; i < world.entities.length; i += 1) {
+          turtle = world.entities[i];
+          this.entities[turtle.name] = new Turtle(turtle);
+        }
+      }
+    };
 
-		TurtleWorld.prototype.addOperations = function (operations) {
-			var i, operation, generatedOperation, step = [];
-			for (i = 0; i < operations.length; i += 1) {
-				operation = operations[i];
-				generatedOperation = this.generateOperation(operation);
-				step.push(generatedOperation);
-			}
-			this.operations.push(step);
-		};
+    TurtleWorld.prototype.getEntity = function(entityID) {
+      return this.entities[entityID];
+    };
 
-		TurtleWorld.prototype.setState = function (state) {
-			var i, j, step;
-			if (state < this.operations.length && state >= -1) {
-				if (this.currentState < state) {
-					for (i = this.currentState + 1; i <= state; i += 1) {
-						step = this.operations[i];
-						for (j = 0; j < step.length; j += 1) {
-							step[j].apply(this);
-						}
-					}
-				} else {
-					for (i = this.currentState; i > state; i -= 1) {
-						step = this.operations[i];
-						for (j = 0; j < step.length; j += 1) {
-							step[j].reverse(this);
-						}
-					}
-				}
-				this.currentState = state;
-			}
-		};
+    TurtleWorld.prototype.clone = function() {
+      return new TurtleWorld(this);
+    };
 
-		TurtleWorld.prototype.generateOperation = function (operation) {
-			switch (operation.type) {
-      case 'moveTurtle':
-        return new MoveTurtle(operation);
-      case 'rotateTurtle':
-        return new RotateTurtle(operation);
-      case 'changeTurtleVisible':
-        return new ChangeTurtleVisible(operation);
-      case 'addLine':
-        return new AddLine(operation);
-      case 'addCircle':
-        return new AddCircle(operation);
-      case 'clearCanvas':
-        return new ClearCanvas(operation);
-      case 'addSizeHint':
-        return new AddSizeHint(operation);
-      default:
-        console.log('Operation not supported yet: ', operation);
-			}
-		};
+    TurtleWorld.prototype.addOperations = function(operations) {
+      var i, operation, generatedOperation, step = [];
+      for (i = 0; i < operations.length; i += 1) {
+        operation = operations[i];
+        generatedOperation = this.generateOperation(operation);
+        step.push(generatedOperation);
+      }
+      this.operations.push(step);
+    };
 
-    TurtleWorld.prototype.addShapes = function (shapes) {
-      var self = this;
-      shapes.forEach(function (shape) {
-        switch (shape.type) {
-        case 'line':
-          self.shapes.push(new Line(shape));
-          break;
-        case 'circle':
-          self.shapes.push(new Circle(shape));
-          break;
+    TurtleWorld.prototype.setState = function(state) {
+      var i, j, step;
+      if (state < this.operations.length && state >= -1) {
+        if (this.currentState < state) {
+          for (i = this.currentState + 1; i <= state; i += 1) {
+            step = this.operations[i];
+            for (j = 0; j < step.length; j += 1) {
+              step[j].apply(this);
+            }
+          }
+        } else {
+          for (i = this.currentState; i > state; i -= 1) {
+            step = this.operations[i];
+            for (j = 0; j < step.length; j += 1) {
+              step[j].reverse(this);
+            }
+          }
+        }
+        this.currentState = state;
+      }
+    };
+
+    TurtleWorld.prototype.generateOperation = function(operation) {
+      switch (operation.name) {
+        case 'moveTurtle':
+          return new MoveTurtle(operation);
+        case 'rotateTurtle':
+          return new RotateTurtle(operation);
+        case 'changeTurtleVisible':
+          return new ChangeTurtleVisible(operation);
+        case 'addLine':
+          return new AddLine(operation);
+        case 'addCircle':
+          return new AddCircle(operation);
+        case 'clearCanvas':
+          return new ClearCanvas(operation);
+        case 'addSizeHint':
+          return new AddSizeHint(operation);
         default:
-          console.log('Shape not supported yet: ', shape);
+          console.log('Operation not supported yet: ', operation);
+      }
+    };
+
+    TurtleWorld.prototype.addShapes = function(shapes) {
+      var self, type;
+      self = this;
+      shapes.forEach(function(shape) {
+        type = shape.type.split('.').pop();
+        switch (type) {
+          case 'Line':
+            self.shapes.push(new Line(shape));
+            break;
+          case 'Circle':
+            self.shapes.push(new Circle(shape));
+            break;
+          default:
+            console.log('Shape not supported yet: ', shape);
         }
       });
     };
-    
-    TurtleWorld.prototype.addSizeHints = function (sizeHints) {
+
+    TurtleWorld.prototype.addSizeHints = function(sizeHints) {
       var self = this;
-      sizeHints.forEach(function (sizeHint) {
+      sizeHints.forEach(function(sizeHint) {
         self.sizeHints.push(new SizeHint(sizeHint));
       });
     };
-    
-		return TurtleWorld;
-	}
+
+    return TurtleWorld;
+  }
 }());
