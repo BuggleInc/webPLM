6bb52d8721c8deb348bf046a689e70bfd48dc39d
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-09-15 18:24:47 +0200

    Clean listenersHandler service

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,6 @@
 (function(){
-	'use strict';
+  'use strict';
 
-	angular
-		.module('PLMApp', ['ngAnimate', 'ngSanitize', 'gettext', 'ngCookies', 'ui.router', 'ui.codemirror', 'angular-locker', 'satellizer', 'toaster', 'angular-bind-html-compile']);
+  angular
+    .module('PLMApp', ['ngAnimate', 'ngSanitize', 'gettext', 'ngCookies', 'ui.router', 'ui.codemirror', 'angular-locker', 'satellizer', 'toaster', 'angular-bind-html-compile']);
 })();
--- public/app/components/bug-report-modal.directive.js
+++ public/app/components/bug-report-modal.directive.js
@@ -1,32 +1,34 @@
 (function(){
-	'use strict';
-	
-	angular
-		.module('PLMApp')
-		.directive('bugReportModal', bugReportModal);
-	
+  'use strict';
+
+  angular
+    .module('PLMApp')
+    .directive('bugReportModal', bugReportModal);
+
   bugReportModal.$inject = ['listenersHandler', 'bugReport'];
-  
-	function bugReportModal (listenersHandler, bugReport) {
-		return {
-			restrict: 'E',
-			templateUrl: '/assets/app/components/bug-report-modal.directive.html',
-			link: function (scope, element, attrs) {
+
+  function bugReportModal (listenersHandler, bugReport) {
+    return {
+      restrict: 'E',
+      templateUrl: '/assets/app/components/bug-report-modal.directive.html',
+      link: function (scope, element, attrs) {
+        var listener = listenersHandler.register('onmessage', handleMessage);
+        scope.$on('$destroy', listener);
+
         scope.bugReport = bugReport;
-        listenersHandler.register('onmessage', handleMessage);
         $(document).foundation('alert', 'reflow');
-				$(document).foundation('reveal', 'reflow');
-			}
-		};
-	}
-  
-  function handleMessage(data) {
-      var cmd = data.cmd;
-      var args = data.args;
-      switch (cmd) {
-        case 'issueCreated':
-          $('#bugReportModal').foundation('reveal', 'close');
-          break;
+        $(document).foundation('reveal', 'reflow');
       }
+    };
+  }
+
+  function handleMessage (data) {
+    var cmd = data.cmd;
+    var args = data.args;
+    switch (cmd) {
+      case 'issueCreated':
+        $('#bugReportModal').foundation('reveal', 'close');
+        break;
+    }
   }
 })();
