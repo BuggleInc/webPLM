f7c9f8a62e146e7a93bb8c4e6d19bdb26146ee98
Martin Quinson <martin.quinson@loria.fr>
2017-10-05 07:35:01 +0200

    whoopse, I forgot to add these files

---
+++ app/json/LectureToJson.scala
@@ -0,0 +1,59 @@
+package json
+
+import models.lesson.{Exercises, Lecture}
+import play.Logger
+import play.api.libs.json.{JsArray, JsValue, Json}
+import plm.core.lang.ProgrammingLanguage
+import plm.core.model.lesson.Exercise
+import plm.core.ui.PlmHtmlEditorKit
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+
+class LectureToJson(exercises: Exercises) {
+
+  type ExercisePassedChecker = (Exercise, ProgrammingLanguage) => Future[Boolean]
+
+  def lectureWrite(
+      lecture: Lecture,
+      languageCode: String,
+      progLang: ProgrammingLanguage,
+      checkedExercisePassed: ExercisePassedChecker): JsValue = {
+    val names: Map[String, String] = lecture.optNames.get
+    val defaultName: String = names("en")
+    val name: String = names.getOrElse(languageCode, defaultName)
+    val exercisePassed: Map[String, Boolean] = generateExercisePassed(lecture.id, checkedExercisePassed)
+
+    Json.obj(
+      "id" -> lecture.id,
+      "name" -> PlmHtmlEditorKit.filterHTML(name, false, progLang),
+      "dependingLectures" -> lecturesWrite(lecture.dependingLectures, languageCode, progLang, checkedExercisePassed),
+      "exercisePassed" -> exercisePassed
+    )
+  }
+
+  def lecturesWrite(
+      lectures: Traversable[Lecture],
+      languageCode: String,
+      progLang: ProgrammingLanguage,
+      checkedExercisePassed: ExercisePassedChecker): JsArray = {
+    JsArray(lectures.map(lectureWrite(_, languageCode, progLang, checkedExercisePassed)).toSeq)
+  }
+
+  private def generateExercisePassed(
+      lectureId: String,
+      checkedExercisePassed: ExercisePassedChecker): Map[String, Boolean] = {
+    exercises.getExercise(lectureId) match {
+      case Some(exercise: Exercise) =>
+        val entries = exercise.getProgLanguages.asScala.view map { lang =>
+          lang.getLang -> Await.result(checkedExercisePassed(exercise, lang), 5 seconds)
+        }
+        entries.toMap
+      case None =>
+        Logger.info(s"exercices $lectureId not found")
+        Map()
+    }
+  }
+}
---
+++ app/json/LessonToJson.scala
@@ -0,0 +1,24 @@
+package json
+
+import models.lesson.Lesson
+import play.api.libs.json.{JsArray, JsValue, Json}
+
+object LessonToJson {
+  def lessonWrite(lesson: Lesson, languageCode: String): JsValue = {
+    val imgPath: String = "lessons/" + lesson.id.replaceAll("\\.", "/") + "/icon.png"
+    val descriptions: Map[String, String] = lesson.descriptions
+    val defaultDescription: String = descriptions("en")
+    val description: String = descriptions.getOrElse(languageCode, defaultDescription)
+
+    Json.obj(
+      "id" -> lesson.id,
+      "name" -> lesson.name,
+      "imgUrl" -> imgPath,
+      "description" -> description
+    )
+  }
+
+  def lessonsWrite(lessons: Traversable[Lesson], languageCode: String): JsArray = {
+    JsArray(lessons.map(lessonWrite(_, languageCode)).toSeq)
+  }
+}
