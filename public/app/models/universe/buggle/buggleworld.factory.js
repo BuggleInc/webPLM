e4fd02204c22f836d7725b1857294b8c64e1170f
Votre Nom <baouz.khaled@gmail.com>
2017-12-14 22:06:30 +0100

    new version of buggleworld, hanoiworld and pancakeWord by deleting the unused functions

--- public/app/models/universe/buggle/buggleworld.factory.js
+++ public/app/models/universe/buggle/buggleworld.factory.js
@@ -1,204 +1,203 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('PLMApp')
-        .factory('BuggleWorld', BuggleWorld);
+  angular
+    .module('PLMApp')
+    .factory('BuggleWorld', BuggleWorld);
 
-    BuggleWorld.$inject = [
-        'BuggleWorldCell', 'Buggle',
-        'ChangeCellHasBaggle', 'ChangeCellColor',
-        'ChangeBuggleBodyColor', 'ChangeBuggleCarryBaggle', 'MoveBuggleOperation', 'ChangeBuggleDirection',
-        'ChangeCellHasContent', 'ChangeCellContent', 'BuggleEncounterWall', 'ChangeBuggleBrushDown',
-        'NoBaggleUnderBuggle', 'BuggleAlreadyHaveBaggle', 'BuggleDontHaveBaggle',
-        'CellAlreadyHaveBaggle', 'BuggleInOuterSpace'
-    ];
+  BuggleWorld.$inject = [
+    'BuggleWorldCell', 'Buggle',
+    'ChangeCellHasBaggle', 'ChangeCellColor',
+    'ChangeBuggleBodyColor', 'ChangeBuggleCarryBaggle', 'MoveBuggleOperation', 'ChangeBuggleDirection',
+    'ChangeCellHasContent', 'ChangeCellContent', 'BuggleEncounterWall', 'ChangeBuggleBrushDown',
+    'NoBaggleUnderBuggle', 'BuggleAlreadyHaveBaggle', 'BuggleDontHaveBaggle',
+    'CellAlreadyHaveBaggle', 'BuggleInOuterSpace'
+  ];
 
-    function BuggleWorld(BuggleWorldCell, Buggle,
-                         ChangeCellHasBaggle, ChangeCellColor,
-                         ChangeBuggleBodyColor, ChangeBuggleCarryBaggle, MoveBuggleOperation, ChangeBuggleDirection,
-                         ChangeCellHasContent, ChangeCellContent, BuggleEncounterWall, ChangeBuggleBrushDown,
-                         NoBaggleUnderBuggle, BuggleAlreadyHaveBaggle, BuggleDontHaveBaggle,
-                         CellAlreadyHaveBaggle, BuggleInOuterSpace) {
+  function BuggleWorld(BuggleWorldCell, Buggle,
+    ChangeCellHasBaggle, ChangeCellColor,
+    ChangeBuggleBodyColor, ChangeBuggleCarryBaggle, MoveBuggleOperation, ChangeBuggleDirection,
+    ChangeCellHasContent, ChangeCellContent, BuggleEncounterWall, ChangeBuggleBrushDown,
+    NoBaggleUnderBuggle, BuggleAlreadyHaveBaggle, BuggleDontHaveBaggle,
+    CellAlreadyHaveBaggle, BuggleInOuterSpace) {
 
-        var BuggleWorld = function (world) {
-            this.type = world.type;
-            this.operations = [];
-            this.currentState = -1;
-            this.steps = [];
+    var BuggleWorld = function (world) {
+      this.type = world.type;
+      this.operations = [];
+      this.currentState = -1;
+      this.steps = [];
 
-            this.cells = [];
-            this.entities = {};
+      this.cells = [];
+      this.entities = {};
 
-            if (world instanceof BuggleWorld) {
-                this.initFromBuggleWorld(world);
-            } else {
-                this.initFromJava(world);
-            }
-        };
+      if (world instanceof BuggleWorld) {
+        this.initFromBuggleWorld(world);
+      } else {
+        this.initFromJava(world);
+      }
+    };
 
-        BuggleWorld.prototype.initFromJava = function (world) {
-            var i, j, cell, buggle;
+    BuggleWorld.prototype.initFromJava = function (world) {
+      var i, j, cell, buggle;
 
-            this.width = world.width;
-            this.height = world.height;
+      this.width = world.width;
+      this.height = world.height;
 
-            // this.initCells();
-            // for (i = 0; i < this.width; i += 1) {
-            //     for (j = 0; j < this.height; j += 1) {
-            //         cell = world.cells[i][j];
-            //         this.cells[i][j] = new BuggleWorldCell(cell);
-            //     }
-            // }
+      this.initCells();
+      for (i = 0; i < this.width; i += 1) {
+        for (j = 0; j < this.height; j += 1) {
+          cell = world.cells[i][j];
+          this.cells[i][j] = new BuggleWorldCell(cell);
+        }
+      }
 
-            for (i = 0; i < world.entities.length; i += 1) {
-                buggle = world.entities[i];
-                this.entities[buggle.name] = new Buggle(buggle);
-            }
-        };
+      for (i = 0; i < world.entities.length; i += 1) {
+        buggle = world.entities[i];
+        this.entities[buggle.name] = new Buggle(buggle);
+      }
+    };
 
-        BuggleWorld.prototype.initFromBuggleWorld = function (world) {
-            var i, j,
-                col, cell,
-                buggleID, buggle;
+    BuggleWorld.prototype.initFromBuggleWorld = function (world) {
+      var i, j,
+        col, cell,
+        buggleID, buggle;
 
-            this.width = world.width;
-            this.height = world.height;
+      this.width = world.width;
+      this.height = world.height;
 
-            // for (i = 0; i < world.width; i += 1) {
-            //     col = [];
-            //     for (j = 0; j < world.height; j += 1) {
-            //         cell = world.getCell(i, j);
-            //         col.push(new BuggleWorldCell(cell));
-            //     }
-            //     this.cells.push(col);
-            // }
+      for (i = 0; i < world.width; i += 1) {
+        col = [];
+        for (j = 0; j < world.height; j += 1) {
+          cell = world.getCell(i, j);
+          col.push(new BuggleWorldCell(cell));
+        }
+        this.cells.push(col);
+      }
 
-            for (buggleID in world.entities) {
-                if (world.entities.hasOwnProperty(buggleID)) {
-                    buggle = world.entities[buggleID];
-                    this.entities[buggleID] = new Buggle(buggle);
-                }
-            }
-        };
+      for (buggleID in world.entities) {
+        if (world.entities.hasOwnProperty(buggleID)) {
+          buggle = world.entities[buggleID];
+          this.entities[buggleID] = new Buggle(buggle);
+        }
+      }
+    };
 
-        BuggleWorld.prototype.clone = function () {
-            return new BuggleWorld(this);
-        };
+    BuggleWorld.prototype.clone = function () {
+      return new BuggleWorld(this);
+    };
 
-        BuggleWorld.prototype.getEntity = function (entityID) {
-            return this.entities[entityID];
-        };
+    BuggleWorld.prototype.getEntity = function (entityID) {
+      return this.entities[entityID];
+    };
 
-        // BuggleWorld.prototype.initCells = function () {
-        //     var i, j, col;
-        //
-        //     for (i = 0; i < this.width; i += 1) {
-        //         col = [];
-        //         for (j = 0; j < this.height; j += 1) {
-        //             col.push(BuggleWorldCell.getDefaultCell(i, j));
-        //         }
-        //         this.cells.push(col);
-        //     }
-        // };
+    BuggleWorld.prototype.initCells = function () {
+      var i, j, col;
 
-        // BuggleWorld.prototype.getCell = function (x, y) {
-        //     return this.cells[x][y];
-        // };
+      for (i = 0; i < this.width; i += 1) {
+        col = [];
+        for (j = 0; j < this.height; j += 1) {
+          col.push(BuggleWorldCell.getDefaultCell(i, j));
+        }
+        this.cells.push(col);
+      }
+    };
 
-        BuggleWorld.prototype.addOperations = function (operations) {
-            var i, step, length, operation, generatedOperation;
+    BuggleWorld.prototype.getCell = function (x, y) {
+      return this.cells[x][y];
+    };
 
-            step = [];
-            length = operations.length;
-            for (i = 0; i < length; i += 1) {
-                operation = operations[i];
-                step.push(operation);
-                // generatedOperation = this.generateOperation(operation);
-                // step.push(generatedOperation);
-            }
-            this.operations.push(step);
-        };
+      BuggleWorld.prototype.addOperations = function (operations) {
+          var i, step, length, operation, generatedOperation;
 
-        BuggleWorld.prototype.setState = function (state) {
-            var i, j, length, step;
-            if (state < this.operations.length && state >= -1) {
+          step = [];
+          length = operations.length;
+          for (i = 0; i < length; i += 1) {
+              operation = operations[i];
+              step.push(operation);
+              // generatedOperation = this.generateOperation(operation);
+              // step.push(generatedOperation);
+          }
+          this.operations.push(step);
+      };
 
-                console.log(this.operations);
+      BuggleWorld.prototype.setState = function (state) {
+          var i, j, length, step;
+          if (state < this.operations.length && state >= -1) {
 
-                if (this.currentState < state) {
-                    for (i = this.currentState + 1; i <= state; i += 1) {
-                        // step = this.operations[i]; On veut récuperer le tableau de toutes les oppérations
-                        step= this.operations;
-                        length = step.length;
-                        // for (j = 0; j < length; j += 1) {
-                        // step[j].apply(this);
-                        this.drawSVG(step[i][0]);
-                        // }
-                    }
-                } else {
-                    for (i = this.currentState; i > state; i -= 1) {
-                        // step = this.operations[i];
-                        step= this.operations;
-                        length = step.length;
-                        // for (j = 0; j < length; j += 1) {
-                        // step[j].reverse(this);
-                        this.drawSVG(step[i][0]);
-                        // }
-                    }
-                }
-                this.currentState = state;
-            }
-        };
+              console.log(this.operations);
 
-        // BuggleWorld.prototype.generateOperation = function (operation) {
-        //     switch (operation.name) {
-        //         case 'moveBuggleOperation':
-        //             return new MoveBuggleOperation(operation);
-        //         case 'changeBuggleBodyColor':
-        //             return new ChangeBuggleBodyColor(operation);
-        //         case 'changeBuggleDirection':
-        //             return new ChangeBuggleDirection(operation);
-        //         case 'changeBuggleCarryBaggle':
-        //             return new ChangeBuggleCarryBaggle(operation);
-        //         case 'changeBuggleBrushDown':
-        //             return new ChangeBuggleBrushDown(operation);
-        //         case 'changeCellColor':
-        //             return new ChangeCellColor(operation);
-        //         case 'changeCellHasBaggle':
-        //             return new ChangeCellHasBaggle(operation);
-        //         case 'changeCellHasContent':
-        //             return new ChangeCellHasContent(operation);
-        //         case 'changeCellContent':
-        //             return new ChangeCellContent(operation);
-        //         case 'buggleEncounterWall':
-        //             return new BuggleEncounterWall(operation);
-        //         case 'noBaggleUnderBuggle':
-        //             return new NoBaggleUnderBuggle(operation);
-        //         case 'buggleAlreadyHaveBaggle':
-        //             return new BuggleAlreadyHaveBaggle(operation);
-        //         case 'buggleDontHaveBaggle':
-        //             return new BuggleDontHaveBaggle(operation);
-        //         case 'cellAlreadyHaveBaggle':
-        //             return new CellAlreadyHaveBaggle(operation);
-        //         case 'buggleInOuterSpace':
-        //             return new BuggleInOuterSpace(operation);
-        //     }
-        // };
+              if (this.currentState < state) {
+                  for (i = this.currentState + 1; i <= state; i += 1) {
+                      // step = this.operations[i]; On veut récuperer le tableau de toutes les oppérations
+                      step= this.operations;
+                      length = step.length;
+                      // for (j = 0; j < length; j += 1) {
+                      // step[j].apply(this);
+                      this.drawSVG(step[i][0]);
+                      // }
+                  }
+              } else {
+                  for (i = this.currentState; i > state; i -= 1) {
+                      // step = this.operations[i];
+                      step= this.operations;
+                      length = step.length;
+                      // for (j = 0; j < length; j += 1) {
+                      // step[j].reverse(this);
+                      this.drawSVG(step[i][0]);
+                      // }
+                  }
+              }
+              this.currentState = state;
+          }
+      };
 
-        BuggleWorld.prototype.drawSVG = function (svg) {
-            (function () {
+    // BuggleWorld.prototype.generateOperation = function (operation) {
+    //   switch (operation.name) {
+    //   case 'moveBuggleOperation':
+    //     return new MoveBuggleOperation(operation);
+    //   case 'changeBuggleBodyColor':
+    //     return new ChangeBuggleBodyColor(operation);
+    //   case 'changeBuggleDirection':
+    //     return new ChangeBuggleDirection(operation);
+    //   case 'changeBuggleCarryBaggle':
+    //     return new ChangeBuggleCarryBaggle(operation);
+    //   case 'changeBuggleBrushDown':
+    //     return new ChangeBuggleBrushDown(operation);
+    //   case 'changeCellColor':
+    //     return new ChangeCellColor(operation);
+    //   case 'changeCellHasBaggle':
+    //     return new ChangeCellHasBaggle(operation);
+    //   case 'changeCellHasContent':
+    //     return new ChangeCellHasContent(operation);
+    //   case 'changeCellContent':
+    //     return new ChangeCellContent(operation);
+    //   case 'buggleEncounterWall':
+    //     return new BuggleEncounterWall(operation);
+    //   case 'noBaggleUnderBuggle':
+    //     return new NoBaggleUnderBuggle(operation);
+    //   case 'buggleAlreadyHaveBaggle':
+    //     return new BuggleAlreadyHaveBaggle(operation);
+    //   case 'buggleDontHaveBaggle':
+    //     return new BuggleDontHaveBaggle(operation);
+    //   case 'cellAlreadyHaveBaggle':
+    //     return new CellAlreadyHaveBaggle(operation);
+    //   case 'buggleInOuterSpace':
+    //     return new BuggleInOuterSpace(operation);
+    //   }
+    // };
+    BuggleWorld.prototype.drawSVG = function (svg) {
+          (function () {
 
-                document.getElementById('drawingArea').innerHTML = svg.operation;
-                var svgbis = document.getElementsByTagName('svg');
-                svgbis[0].setAttribute("width", "400px");
-                svgbis[0].setAttribute("height", "400px");
+              document.getElementById('drawingArea').innerHTML = svg.operation;
+              var svgbis = document.getElementsByTagName('svg');
+              svgbis[0].setAttribute("width", "400px");
+              svgbis[0].setAttribute("height", "400px");
 
 
-            })();
+          })();
 
-        };
+      };
 
-        return BuggleWorld;
-    }
+    return BuggleWorld;
+  }
 }());
--- public/app/models/universe/buggle/buggleworldview.factory.js
+++ public/app/models/universe/buggle/buggleworldview.factory.js
@@ -15,7 +15,8 @@
 
     var cellWidth;
     var cellHeight;
-    var INVADER_SPRITE_SIZE = 11;
+
+    // var INVADER_SPRITE_SIZE = 11;
     // var INVADER_SPRITE = [
     //   [
     //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -71,11 +72,11 @@
     //   ]
     // ];
 
-    // var service = {
-    //   draw: draw,
-    // };
+    var service = {
+      draw: draw,
+    };
 
-   // return service;
+    return service;
 
     function initUtils(canvas, buggleWorld) {
       ctx = canvas.getContext('2d');
@@ -89,168 +90,139 @@
       // } else {
       //   cellHeight = cellWidth;
       // }
-      canvas.width =  buggleWorld.width;
-      canvas.height =  buggleWorld.height;
+      canvas.width = cellWidth * buggleWorld.width;
+      canvas.height = cellHeight * buggleWorld.height;
     }
 
     function draw(canvas, buggleWorld) {
-      // var i, j;
-      // var buggleID;
-      //
-      // //console.log('buggleworld : ', buggleWorld);
-      //
-      // initUtils(canvas, buggleWorld);
-      //
-      // for (i = 0; i < buggleWorld.width; i++) {
-      //     for (j = 0; j < buggleWorld.height; j++) {
-      //       drawCell(buggleWorld.cells[i][j]);
-      //     }
-      //
-      //   }
-      //
-      //   drawGrid(buggleWorld);
-      //   drawFrontierWalls(buggleWorld);
-      //
-      //   for (buggleID in buggleWorld.entities) {
-      //     if (buggleWorld.entities.hasOwnProperty(buggleID)) {
-      //       var buggle = buggleWorld.entities[buggleID];
-      //       var cell = buggleWorld.cells[buggle.x][buggle.y]
-      //       drawBuggle(buggle, cell);
-      //     }
-      //   }
+      var i, j;
+      var buggleID;
 
-
-        //console.log(buggleWorld.SVGOperations[0].operation)
-        //var svg = buggleWorld.SVGOperations.operation;
-        //
-        //document.getElementById('imageSVG').innerHTML = svg;
-
-
-
+      initUtils(canvas, buggleWorld);
 
       // for (i = 0; i < buggleWorld.width; i++) {
       //   for (j = 0; j < buggleWorld.height; j++) {
       //     drawCell(buggleWorld.cells[i][j]);
       //   }
       // }
-      //
+
       // drawGrid(buggleWorld);
       // drawFrontierWalls(buggleWorld);
-      //
-      // for (buggleID in buggleWorld.entities) {
-      //   if (buggleWorld.entities.hasOwnProperty(buggleID)) {
-      //     var buggle = buggleWorld.entities[buggleID];
-      //     var cell = buggleWorld.cells[buggle.x][buggle.y]
-      //     drawBuggle(buggle, cell);
-      //   }
-      // }
+
+      for (buggleID in buggleWorld.entities) {
+        if (buggleWorld.entities.hasOwnProperty(buggleID)) {
+          var buggle = buggleWorld.entities[buggleID];
+          // var cell = buggleWorld.cells[buggle.x][buggle.y]
+          // drawBuggle(buggle, cell);
+        }
+      }
     }
 
-    // function drawCell(cell) {
-    //   var xLeft = cellWidth * cell.x;
-    //   var yTop = cellHeight * cell.y;
-    //
-    //   var padX = cellWidth / 2;
-    //   var padY = cellHeight / 2;
-    //
-    //   var xRight = cellWidth * (cell.x + 1);
-    //   var yBottom = cellHeight * (cell.y + 1);
-    //
-    //   ctx.beginPath();
-    //   ctx.lineWidth = 4;
-    //   ctx.strokeStyle = 'SteelBlue';
-    //   ctx.fillStyle = 'rgba(' + cell.color.join(',') + ')';
-    //   if (cell.color[0] === 255 && cell.color[1] === 255 && cell.color[2] === 255 && cell.color[3] === 255) {
-    //     if ((cell.x + cell.y) % 2 === 0) {
-    //       ctx.fillStyle = 'rgb(230, 230, 230)';
-    //     } else {
-    //       ctx.fillStyle = 'rgb(255, 255, 255)';
-    //     }
-    //   }
-    //   ctx.fillRect(xLeft, yTop, xRight, yBottom);
-    //
-    //   if (cell.leftWall) {
-    //     ctx.moveTo(xLeft, yTop);
-    //     ctx.lineTo(xLeft, yBottom);
-    //   }
-    //   if (cell.topWall) {
-    //     ctx.moveTo(xLeft, yTop);
-    //     ctx.lineTo(xRight, yTop);
-    //   }
-    //
-    //   ctx.stroke();
-    //   ctx.closePath();
-    //
-    //   if (cell.hasBaggle) {
-    //     ctx.beginPath();
-    //     ctx.fillStyle = DefaultColors.BAGGLE;
-    //     ctx.arc(xLeft + padX, yTop + padY, cellWidth * 0.25, 0, Math.PI * 2, true);
-    //     ctx.arc(xLeft + padX, yTop + padY, cellWidth * 0.15, 0, Math.PI * 2, true);
-    //     ctx.fill('evenodd');
-    //     ctx.closePath();
-    //   }
-    //   ctx.beginPath();
-    //   ctx.fillStyle = DefaultColors.MESSAGE_COLOR;
-    //   ctx.fillText(cell.content, xLeft + 1, yBottom - 4);
-    //   ctx.closePath();
-    // }
-
-    // function drawGrid(buggleWorld) {
-    //   var i, j;
-    //
-    //   ctx.beginPath();
-    //   ctx.lineWidth = 1;
-    //   ctx.strokeStyle = 'grey';
-    //   for (i = 0; i <= buggleWorld.width; i++) {
-    //     ctx.moveTo(i * cellWidth, 0);
-    //     ctx.lineTo(i * cellWidth, canvasHeight);
-    //   }
-    //   for (j = 0; j <= buggleWorld.height; j++) {
-    //     ctx.moveTo(0, j * cellHeight);
-    //     ctx.lineTo(canvasWidth, j * cellHeight);
-    //   }
-    //   ctx.stroke();
-    //   ctx.closePath();
-    // }
-
-    // function drawFrontierWalls(buggleWorld) {
-    //   var x, y;
-    //
-    //   var xLeft;
-    //   var xRight;
-    //   var yTop;
-    //   var yBottom;
-    //
-    //   ctx.beginPath();
-    //
-    //   ctx.lineWidth = 4;
-    //   ctx.strokeStyle = 'SteelBlue';
-    //
-    //   // frontier walls (since the world is a torus)
-    //   for (y = 0; y < buggleWorld.height; y++) {
-    //     if (buggleWorld.cells[0][y].leftWall) {
-    //       xLeft = canvasWidth;
-    //       yTop = cellHeight * y;
-    //       yBottom = cellHeight * (y + 1);
-    //       ctx.moveTo(xLeft, yTop);
-    //       ctx.lineTo(xLeft, yBottom);
-    //     }
-    //   }
-    //
-    //   for (x = 0; x < buggleWorld.width; x++) {
-    //     if (buggleWorld.cells[x][0].topWall) {
-    //       xLeft = cellWidth * x;
-    //       xRight = cellWidth * (x + 1);
-    //       yTop = canvasHeight;
-    //       ctx.moveTo(xLeft, yTop);
-    //       ctx.lineTo(xRight, yTop);
-    //     }
-    //   }
-    //
-    //   ctx.stroke();
-    //   ctx.closePath();
-    // }
-
+  //   function drawCell(cell) {
+  //     var xLeft = cellWidth * cell.x;
+  //     var yTop = cellHeight * cell.y;
+  //
+  //     var padX = cellWidth / 2;
+  //     var padY = cellHeight / 2;
+  //
+  //     var xRight = cellWidth * (cell.x + 1);
+  //     var yBottom = cellHeight * (cell.y + 1);
+  //
+  //     ctx.beginPath();
+  //     ctx.lineWidth = 4;
+  //     ctx.strokeStyle = 'SteelBlue';
+  //     ctx.fillStyle = 'rgba(' + cell.color.join(',') + ')';
+  //     if (cell.color[0] === 255 && cell.color[1] === 255 && cell.color[2] === 255 && cell.color[3] === 255) {
+  //       if ((cell.x + cell.y) % 2 === 0) {
+  //         ctx.fillStyle = 'rgb(230, 230, 230)';
+  //       } else {
+  //         ctx.fillStyle = 'rgb(255, 255, 255)';
+  //       }
+  //     }
+  //     ctx.fillRect(xLeft, yTop, xRight, yBottom);
+  //
+  //     if (cell.leftWall) {
+  //       ctx.moveTo(xLeft, yTop);
+  //       ctx.lineTo(xLeft, yBottom);
+  //     }
+  //     if (cell.topWall) {
+  //       ctx.moveTo(xLeft, yTop);
+  //       ctx.lineTo(xRight, yTop);
+  //     }
+  //
+  //     ctx.stroke();
+  //     ctx.closePath();
+  //
+  //     if (cell.hasBaggle) {
+  //       ctx.beginPath();
+  //       ctx.fillStyle = DefaultColors.BAGGLE;
+  //       ctx.arc(xLeft + padX, yTop + padY, cellWidth * 0.25, 0, Math.PI * 2, true);
+  //       ctx.arc(xLeft + padX, yTop + padY, cellWidth * 0.15, 0, Math.PI * 2, true);
+  //       ctx.fill('evenodd');
+  //       ctx.closePath();
+  //     }
+  //     ctx.beginPath();
+  //     ctx.fillStyle = DefaultColors.MESSAGE_COLOR;
+  //     ctx.fillText(cell.content, xLeft + 1, yBottom - 4);
+  //     ctx.closePath();
+  //   }
+  //
+  //   function drawGrid(buggleWorld) {
+  //     var i, j;
+  //
+  //     ctx.beginPath();
+  //     ctx.lineWidth = 1;
+  //     ctx.strokeStyle = 'grey';
+  //     for (i = 0; i <= buggleWorld.width; i++) {
+  //       ctx.moveTo(i * cellWidth, 0);
+  //       ctx.lineTo(i * cellWidth, canvasHeight);
+  //     }
+  //     for (j = 0; j <= buggleWorld.height; j++) {
+  //       ctx.moveTo(0, j * cellHeight);
+  //       ctx.lineTo(canvasWidth, j * cellHeight);
+  //     }
+  //     ctx.stroke();
+  //     ctx.closePath();
+  //   }
+  //
+  //   function drawFrontierWalls(buggleWorld) {
+  //     var x, y;
+  //
+  //     var xLeft;
+  //     var xRight;
+  //     var yTop;
+  //     var yBottom;
+  //
+  //     ctx.beginPath();
+  //
+  //     ctx.lineWidth = 4;
+  //     ctx.strokeStyle = 'SteelBlue';
+  //
+  //     // frontier walls (since the world is a torus)
+  //     for (y = 0; y < buggleWorld.height; y++) {
+  //       if (buggleWorld.cells[0][y].leftWall) {
+  //         xLeft = canvasWidth;
+  //         yTop = cellHeight * y;
+  //         yBottom = cellHeight * (y + 1);
+  //         ctx.moveTo(xLeft, yTop);
+  //         ctx.lineTo(xLeft, yBottom);
+  //       }
+  //     }
+  //
+  //     for (x = 0; x < buggleWorld.width; x++) {
+  //       if (buggleWorld.cells[x][0].topWall) {
+  //         xLeft = cellWidth * x;
+  //         xRight = cellWidth * (x + 1);
+  //         yTop = canvasHeight;
+  //         ctx.moveTo(xLeft, yTop);
+  //         ctx.lineTo(xRight, yTop);
+  //       }
+  //     }
+  //
+  //     ctx.stroke();
+  //     ctx.closePath();
+  //   }
+  //
   //   function drawBuggle(buggle, cell) {
   //     var dx, dy;
   //
