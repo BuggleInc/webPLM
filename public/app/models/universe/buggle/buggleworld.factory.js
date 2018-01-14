f1a96a946e67ba8faf048aa409b30a8ce687a427
Martin Quinson <martin.quinson@loria.fr>
2018-01-14 15:46:18 +0100

    actually, buggle cells and entities can die too in javascript

--- public/app/models/universe/buggle/buggleworld.factory.js
+++ public/app/models/universe/buggle/buggleworld.factory.js
@@ -5,11 +5,7 @@
     .module('PLMApp')
     .factory('BuggleWorld', BuggleWorld);
 
-  BuggleWorld.$inject = [
-    'BuggleWorldCell', 'Buggle'
-  ];
-
-  function BuggleWorld(BuggleWorldCell, Buggle) {
+  function BuggleWorld() {
 
     var BuggleWorld = function (world) {
       this.type = world.type;
@@ -17,83 +13,12 @@
       this.currentState = -1;
       this.steps = [];
 
-      this.cells = [];
-      this.entities = {};
-
-      if (world instanceof BuggleWorld) {
-        this.initFromBuggleWorld(world);
-      } else {
-        this.initFromJava(world);
-      }
-    };
-
-    BuggleWorld.prototype.initFromJava = function (world) {
-      var i, j, cell, buggle;
-
       this.width = world.width;
       this.height = world.height;
-
-      this.initCells();
-      for (i = 0; i < this.width; i += 1) {
-        for (j = 0; j < this.height; j += 1) {
-          cell = world.cells[i][j];
-          this.cells[i][j] = new BuggleWorldCell(cell);
-        }
-      }
-
-      for (i = 0; i < world.entities.length; i += 1) {
-        buggle = world.entities[i];
-        this.entities[buggle.name] = new Buggle(buggle);
-      }
-    };
-
-    BuggleWorld.prototype.initFromBuggleWorld = function (world) {
-      var i, j,
-        col, cell,
-        buggleID, buggle;
-
-      this.width = world.width;
-      this.height = world.height;
-
-      for (i = 0; i < world.width; i += 1) {
-        col = [];
-        for (j = 0; j < world.height; j += 1) {
-          cell = world.getCell(i, j);
-          col.push(new BuggleWorldCell(cell));
-        }
-        this.cells.push(col);
-      }
-
-      for (buggleID in world.entities) {
-        if (world.entities.hasOwnProperty(buggleID)) {
-          buggle = world.entities[buggleID];
-          this.entities[buggleID] = new Buggle(buggle);
-        }
-      }
     };
 
     BuggleWorld.prototype.clone = function () {
       return new BuggleWorld(this);
-    };
-
-    BuggleWorld.prototype.getEntity = function (entityID) {
-      return this.entities[entityID];
-    };
-
-    BuggleWorld.prototype.initCells = function () {
-      var i, j, col;
-
-      for (i = 0; i < this.width; i += 1) {
-        col = [];
-        for (j = 0; j < this.height; j += 1) {
-          col.push(BuggleWorldCell.getDefaultCell(i, j));
-        }
-        this.cells.push(col);
-      }
-    };
-
-    BuggleWorld.prototype.getCell = function (x, y) {
-      return this.cells[x][y];
     };
 
       BuggleWorld.prototype.addOperations = function (operations) {
