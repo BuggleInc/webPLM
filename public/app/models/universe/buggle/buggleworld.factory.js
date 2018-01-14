6e4fc15bfe88629316f4ad095bf6c4ce14b8759e
Martin Quinson <martin.quinson@loria.fr>
2018-01-14 15:52:29 +0100

    kill dead code

--- public/app/models/universe/buggle/buggleworld.factory.js
+++ public/app/models/universe/buggle/buggleworld.factory.js
@@ -29,8 +29,6 @@
           for (i = 0; i < length; i += 1) {
               operation = operations[i];
               step.push(operation);
-              // generatedOperation = this.generateOperation(operation);
-              // step.push(generatedOperation);
           }
           this.operations.push(step);
       };
@@ -41,23 +39,15 @@
 
               if (this.currentState < state) {
                   for (i = this.currentState + 1; i <= state; i += 1) {
-                      // step = this.operations[i];
                       step= this.operations;
                       length = step.length;
-                      // for (j = 0; j < length; j += 1) {
-                      // step[j].apply(this);
                       this.drawSVG(step[i][0]);
-                      // }
                   }
               } else {
                   for (i = this.currentState; i > state; i -= 1) {
-                      // step = this.operations[i];
                       step= this.operations;
                       length = step.length;
-                      // for (j = 0; j < length; j += 1) {
-                      // step[j].reverse(this);
                       this.drawSVG(step[i][0]);
-                      // }
                   }
               }
               this.currentState = state;
@@ -72,9 +62,7 @@
               svgbis[0].setAttribute("width", "400px");
               svgbis[0].setAttribute("height", "400px");
 
-
           })();
-
       };
 
     return BuggleWorld;
